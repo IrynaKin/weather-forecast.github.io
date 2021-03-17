468aca4a0e988691247f080d0ce91fff2bfab7d1
Iryna <iryna.kin@me.com>
2021-03-17 11:19:07 +0200

    fixed icon

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 
 
 
-    fetch('http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19').then(function (res) {
-    return res.json() }).then(function (data) {
+    fetch('http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19')
+    .then(res => res.json())
+    .then(data =>{
     document.querySelector('.city').textContent = data.name;
-    document.querySelector('.icon').innerHTML = `<img src="http://openweathermap.org/img/w/10d.png">`;
+    document.querySelector('.icon').innerHTML = `<img src="http://openweathermap.org/img/w/${data.weather[0].icon}.png">`;
     document.querySelector('.temp').innerHTML = "Temperature: " + data.main.temp + '&deg;'+"C";
     document.querySelector(".pressure").innerHTML = "Pressure: " + data.main.pressure + " hPa";
     document.querySelector(".humidity").innerHTML ="Humidity: " + data.main.humidity + "%";
